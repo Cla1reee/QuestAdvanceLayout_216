1f8cc354c613089edb68e7be7aab1c2c3dd5e378
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-10-24 01:02:26 +0700

    membuat card view data diri

--- app/src/main/java/com/example/myapplication/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/UIPertama.kt
@@ -22,5 +22,39 @@ fun AktivitasPertama(modifier: Modifier) {
             fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height(height = 25.dp))
+        Card (modifier = Modifier
+            .fillMaxWidth(fraction = 1f)
+            .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )){
+            Row {
+                val gambar = painterResource( id = R.drawable.logo_umy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 100.dp)
+                        .padding(all = 5.dp)
+                )
+                Spacer(modifier = Modifier.width(width = 30.dp))
+                Column(){
+                    Text(
+                        text =  stringResource (id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Default,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+
+                    )
+                    Text(
+                        text = stringResource(id = R.string.alamat),
+                        fontSize = 20.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                }
+            }
+        }
     }
 }
